f9ffe9ec9654989190a4f8b92890eb2b17f71cd3
Max Graff <maksymilian.graff@gmail.com>
2024-10-15 17:31:57 +0200

    previous main program loaded into function; creating a new one;

--- example/main.c
+++ example/main.c
@@ -1,7 +1,9 @@
 #include "icm20948_i2c.h"
 #include <stdio.h>
 
+void printICM_data(ICM20948* icm);
 
+void printExactGyroAngle(ICM20948* icm);
 
 int main()
 {
@@ -26,8 +28,51 @@ int main()
     ICM20948* icm1 = createICM20948(i2c0, 1);
 	printf("ICM created\n");
 
-	ICM20948_Init(icm1);
+	printExactGyroAngle(icm1);
 
+	
+
+	return 1;
+}
+
+void printExactGyroAngle(ICM20948* icm)
+{
+
+	ICM20948_Init(icm);
+	ICM20948_ODR_ALIGN_enable(icm);
+	ICM20948_set_GYRO_SAMPLE_RATE_DIV(icm, (uint8_t)16);
+	ICM20948_set_ACCEL_SAMPLE_RATE_DIV(icm, 255);
+
+	float gyro_data_ms = 0;
+	float accel_data_ms = 0;
+
+	for(int i = 0; i < 2; i++)
+	{
+		gyro_data_ms = (ICM20948_get_GYRO_ODR_kHz(icm)*1000);
+		accel_data_ms = (ICM20948_get_ACCEL_ODR_kHz(icm)*1000);
+
+		printf("GYRO: %f ms; ACCEL: %f ms \n", gyro_data_ms, accel_data_ms);
+
+	}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+void printICM_data(ICM20948* icm)
+{
+	ICM20948_Init(icm);
 	float gx = 0;
 	float gy = 0;
 	float gz = 0;
@@ -40,21 +85,23 @@ int main()
 
 	for(int i = 0; i < 100; i++)
 	{
-		gx = ICM20948_GYRO_raw_to_dps(icm1, ICM20948_get_GYRO_X_raw(icm1));
-		gy = ICM20948_GYRO_raw_to_dps(icm1, ICM20948_get_GYRO_Y_raw(icm1));
-		gz = ICM20948_GYRO_raw_to_dps(icm1, ICM20948_get_GYRO_Z_raw(icm1));
+		gx = ICM20948_GYRO_raw_to_dps(icm, ICM20948_get_GYRO_X_raw(icm));
+		gy = ICM20948_GYRO_raw_to_dps(icm, ICM20948_get_GYRO_Y_raw(icm));
+		gz = ICM20948_GYRO_raw_to_dps(icm, ICM20948_get_GYRO_Z_raw(icm));
 
-		ax = ICM20948_ACCEL_raw_to_g(icm1, ICM20948_get_ACCEL_X_raw(icm1));
-		ay = ICM20948_ACCEL_raw_to_g(icm1, ICM20948_get_ACCEL_Y_raw(icm1));
-		az = ICM20948_ACCEL_raw_to_g(icm1, ICM20948_get_ACCEL_Z_raw(icm1));
+		ax = ICM20948_ACCEL_raw_to_g(icm, ICM20948_get_ACCEL_X_raw(icm));
+		ay = ICM20948_ACCEL_raw_to_g(icm, ICM20948_get_ACCEL_Y_raw(icm));
+		az = ICM20948_ACCEL_raw_to_g(icm, ICM20948_get_ACCEL_Z_raw(icm));
 
 		printf("GYRO: x %f; y %f; z %f;\t\t ACCEL: x %f; y %f; z %f;\t\t TEMP: %f C\n",
 		 	gx, gy, gz,
 			ax, ay, az,
-			ICM20948_get_TEMP_C(icm1)
+			ICM20948_get_TEMP_C(icm)
 		);
 		sleep_ms(1000);
 	}
 
-	return 1;
+
+
+
 }
